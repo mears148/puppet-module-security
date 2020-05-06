5af6e9a43c38f8256837f549145e568581d598e5
mears148 <johnsmears@gmail.com>
2020-05-06 13:35:59 -0400

    add ssh config to ec2 linux instances

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -3,6 +3,16 @@
 #
 class security::ssh {
 
-  include ssh
+  class { 'ssh':
+    sshd_config_hostkey               => [ '/etc/ssh/ssh_host_rsa_key', '/etc/ssh/ssh_host_ecdsa_key', '/etc/ssh/ssh_host_ed25519_key' ],
+    sshd_config_syslog_facility       => 'AUTHPRIV',
+    sshd_config_login_grace_time      => '2m',
+    sshd_config_authkey_location      => '.ssh/authorized_keys',
+    sshd_password_authentication      => 'yes',
+    sshd_config_challenge_resp_auth   => 'no',
+    sshd_gssapicleanupcredentials     => 'no',
+    sshd_authorized_keys_command      => '/opt/aws/bin/eic_run_authorized_keys %u %f',
+    sshd_authorized_keys_command_user => 'ec2-instance-connect',
+  }
 
 }
