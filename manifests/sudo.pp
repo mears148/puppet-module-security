ffb9d769c793bea20cd56e65ee2e6596aabb12ae
mears148 <johnsmears@gmail.com>
2020-05-13 12:00:39 -0400

    add option to opt out of admin_common sudo rule

--- manifests/sudo.pp
+++ manifests/sudo.pp
@@ -1,7 +1,9 @@
 # Class: security::sudo
 #
 #
-class security::sudo {
+class security::sudo (
+  Array $admin_all_only,
+) {
 
   include sudo
 
@@ -13,8 +15,15 @@ class security::sudo {
     content => 'brick ALL=(ALL) NOPASSWD: ALL',
   }
 
-  sudo::conf { 'JAWS_Server_Admin_Common':
-    content => '%jaws_server_admin_common@jaws-sdbx.com ALL=(ALL) NOPASSWD: ALL',
+  if $facts['ec2_metadata']['instance-id'] == $admin_all_only {
+    sudo::conf { 'JAWS_Server_Admin_ALL':
+      content => '%jaws_server_admin_all@jaws-sdbx.com ALL=(ALL) NOPASSWD: ALL',
+    }
+  }
+  else {
+    sudo::conf { 'JAWS_Server_Admin_Common':
+      content => '%jaws_server_admin_common@jaws-sdbx.com ALL=(ALL) NOPASSWD: ALL',
+    }
   }
 
 }
